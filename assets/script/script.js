33d711645340549dbbfb5dde25d1cedffa27de91
Alexis-Thibault <alexis.thibault45150@laposte.net>
2023-03-16 11:22:24 +0100

    created platfroms

--- assets/script/script.js
+++ assets/script/script.js
@@ -27,9 +27,9 @@ function preload() {
         frameWidth: 1200, frameHeight: 700
     });
     this.load.image('simpson', './assets/images/simpson.png')
-    this.load.image('obstacle1', './assets/images/obstacle1.png')
-    this.load.image('obstacle2', './assets/images/obstacle2.png')
-    this.load.image('obstacle3', './assets/images/obstacle3.png')
+    this.load.image('platform1', './assets/images/platform1.png')
+    this.load.image('platform2', './assets/images/platform2.png')
+    this.load.image('platform3', './assets/images/platform3.png')
     this.load.image('donut', './assets/images/donut.png',
         { frameWidth: 50, frameHeight: 50 });
 }
@@ -38,14 +38,14 @@ function create() {
     this.add.image(0, 0, 'space').setOrigin(0, 0);
 
     platforms = this.physics.add.staticGroup();
-    platforms.create(150, 500, 'obstacle2');
-    platforms.create(600, 200, 'obstacle3');
-    platforms.create(1000, 400, 'obstacle1');
+    platforms.create(150, 500, 'platform2');
+    platforms.create(600, 200, 'platform3');
+    platforms.create(1000, 400, 'platform1');
 
     simpson = this.physics.add.image(200, 200, 'simpson');
     simpson.body.collideWorldBounds = true;
     this.physics.add.collider(simpson, platforms);
-    
+
     cursors = this.input.keyboard.createCursorKeys()
 }
 
